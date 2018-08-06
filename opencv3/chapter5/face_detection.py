b2c0c2e5112aea22c83700d364aa7cde78572831
xusheng <klein203@hotmail.com>
2018-08-06 18:24:45 +0800

    bugfix: eyes detection

--- opencv3/chapter5/face_detection.py
+++ opencv3/chapter5/face_detection.py
@@ -11,7 +11,7 @@ def detect():
     camera = cv2.VideoCapture(0)
     size = (camera.get(cv2.CAP_PROP_FRAME_WIDTH), camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
     
-    cnt = 1
+#     cnt = 1
     while True:
         _, frame = camera.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -19,19 +19,20 @@ def detect():
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         
         for (x, y, w, h) in faces:
-            img = cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0))
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0))
             
+            roi_color = frame[y:y+h, x:x+w]
             roi_gray = gray[y:y+h, x:x+w]
             
             # resize POI to (200, 200), and save to disk
-            f = cv2.resize(roi_gray, (200, 200))
-            cv2.imwrite(os.path.join('..', 'data', 'at', 'jm', ('%s.pgm' % cnt)), f)
-            cnt += 1
+#             f = cv2.resize(roi_gray, (200, 200))
+#             cv2.imwrite(os.path.join('..', 'data', 'at', 'jm', ('%s.pgm' % cnt)), f)
+#             cnt += 1
                         
             eyes = eye_cascade.detectMultiScale(roi_gray, 1.03, 5, 0, (40, 40))
             
             for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(img, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (0, 255, 0), 2)
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
         
         cv2.imshow(("camera_%sx%s" % (size[0], size[1])), frame)
         if cv2.waitKey(1000//12) & 0xFF == ord("q"):
