0f64884e4976d7ef9aacacbc39ae1ee36ab7b9ef
xusheng <klein203@hotmail.com>
2018-10-17 16:31:26 +0800

    ex6 completed

--- CS_Machine_Learning/ex6/dataset3Params.m
+++ CS_Machine_Learning/ex6/dataset3Params.m
@@ -24,9 +24,24 @@ sigma = 0.3;
 %
 
 
+pred_error = 1e10;	% init an extremely big number
 
+C_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigma_vec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
-
+for i = 1:length(C_vec)
+	for j = 1:length(sigma_vec)
+		model = svmTrain(X, y, C_vec(i), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(j)));
+		pred_val = svmPredict(model, Xval);
+		
+		cur_error = mean(double(pred_val ~= yval));
+		if cur_error < pred_error
+			pred_error = cur_error;
+			C = C_vec(i);
+			sigma = sigma_vec(j);
+		end
+	end
+end
 
 
 % =========================================================================
